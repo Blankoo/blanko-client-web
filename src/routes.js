d9142b0456a77458da1182f5a0f8c71aa9889338
info@noudadrichem.com <info@noudadrichem.com>
2018-11-25 15:28:51 +0100

    started on login page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from './containers/App'
-import Home from './components/Home'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 export default (
-  <Route path="/" component={App}>
+	<Route path="/" component={App}>
     <IndexRoute component={Home} />
+		<Route path="/login" component={Login}></Route>
   </Route>
 )
