0c3ce217bc030885ca2da7d1cc865cd9be184c23
Donovan Roubos <donovanroubos@MBP-van-Donovan.fritz.box>
2019-02-04 20:59:27 +0100

    Change Home id to class and component Scss cleanup

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
-const axiosInstance = axios.create();
+const axiosInstance = axios.create()
 
 axiosInstance.interceptors.request.use(config => {
 	const token = localStorage.getItem('USER_TOK')
+
 	if(token) {
 		config.headers.Authorization = `Bearer ${token}`
 	}
+
 	return config;
 }, error => {
 	throw error
-});
+})
 
-export default axiosInstance;
+export default axiosInstance
