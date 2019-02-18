9bda6e13b34f499383206a9bf9d8156483bceb4d
info@noudadrichem.com <info@noudadrichem.com>
2019-02-18 19:10:09 +0100

    time mes core don

--- src/components/TimeMeasuring/index.js
+++ src/components/TimeMeasuring/index.js
@@ -7,6 +7,10 @@ import {
 } from '../../actions'
 
 import SingleMeasurement from './SingleMeasurement'
+import {
+  secondsToHourMinuteSecond,
+  totalInSeconds
+} from './actions'
 
 class TimeMeasuring extends React.Component {
 	state = {
@@ -57,17 +61,7 @@ class TimeMeasuring extends React.Component {
 		})
 	}
 
-	secondsToHourMinuteSecond = totalSeconds => {
-		const hour = Math.floor(totalSeconds / 3600)
-		const minute = Math.floor(totalSeconds % 3600 / 60)
-		const seconds = Math.floor(totalSeconds % 3600 % 60)
-
-		return `${('0' + hour).slice(-2)}:${('0' + minute).slice(-2)}:${('0' + seconds).slice(-2)}`
-	}
-
 	render() {
-		const totalInMiliSeconds = (endTime, startTime) => Math.floor(endTime - startTime)
-		const totalInSeconds = (endTime, startTime) => Math.floor(totalInMiliSeconds(endTime, startTime) / 1000)
     // const totalMeasuredTime = this.props.measurements.reduce((zero, { total }) => zero + total, 0)
     const { activeTaskId, measurements } = this.props
     const { startTime, currenTime } = this.state
@@ -79,7 +73,7 @@ class TimeMeasuring extends React.Component {
           <button onClick={() => this.stopMeasurement(activeTaskId)}>Stop</button>
 					<span className="numbers">
 					{
-						this.secondsToHourMinuteSecond(
+						secondsToHourMinuteSecond(
 							totalInSeconds(currenTime, startTime)
 						)
 					}
