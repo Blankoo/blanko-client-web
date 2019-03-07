9b979e91c862658d84c891b7fe89ed158ead324c
noudadrichem <info@noudadrichem.com>
2019-03-07 18:23:18 +0100

    task detail measurement styling

--- src/components/TimeMeasuring/SingleMeasurement.js
+++ src/components/TimeMeasuring/SingleMeasurement.js
@@ -4,13 +4,9 @@ import { secondsToHourMinuteSecond } from './actions'
 const SingleMeasurment = ({ total }) => (
   <div className="single-measurement">
     <span>Measurement:</span>
-    <span>{ secondsToHourMinuteSecond(total / 1000) }</span>
+    <span className="mono">{ secondsToHourMinuteSecond(total / 1000) }</span>
   </div>
 )
 
 
 export default SingleMeasurment
-
-// start = 1550511484781
-// end = 1550511526983
-// 1550511526983 - 1550511484781
