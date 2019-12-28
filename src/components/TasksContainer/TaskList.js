7143223a5d1a5e0f019f1fd9ca8b1f6c3b0885de
noudadrichem <info@noudadrichem.com>
2019-12-28 17:57:17 +0100

    uh ok

--- src/components/TasksContainer/TaskList.js
+++ src/components/TasksContainer/TaskList.js
@@ -8,139 +8,142 @@ import { reorderTasks } from '../../actions'
 import Task from '../Task'
 import AddTask from '../AddTask'
 
+/*eslint-disable */
 const ChevronRight = ({ size = 16, color = "#7A848F", className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={`chevron-right ${className}`} width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="3" strokeLinecap="square" strokeLinejoin="arcs"><path d="M9 18l6-6-6-6" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={`chevron-right ${className}`} width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="3" strokeLinecap="square" strokeLinejoin="arcs"><path d="M9 18l6-6-6-6" /></svg>
 )
+/*eslint-enable */
 
 class TaskList extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      open: !props.deleted
+        this.state = {
+            open: !props.deleted
+        }
     }
-  }
 
-  filterByQuery = (task) => {
-    const { title, subTitle } = task
-    const { searchQuery } = this.props
+    filterByQuery = (task) => {
+        const { title } = task
+        const { searchQuery } = this.props
 
-    return (
-      title.toLowerCase().includes(searchQuery) ||
-      subTitle.toLowerCase().includes(searchQuery)
-    )
-  }
-
-  reOrderOnDragEnd = (draggingResources) => {
-    const { source, destination, reason, draggableId } = draggingResources
-
-    if(!draggingResources.destination) {
-      return
-    } else if(reason === 'DROP') {
-      this.props.reorderTasks(
-        this.props.deleted,
-        this.props.deleted ? this.props.archivedTasks : this.props.tasks,
-        source,
-        destination,
-        draggableId,
-        this.props.projectId
-      )
+        return title.toLowerCase().includes(searchQuery)
     }
-  }
 
-  sortTasksByOrder = (a, b) => a.order - b.order
+    reOrderOnDragEnd = (draggingResources) => {
+        const { source, destination, reason, draggableId } = draggingResources
 
-  toggleHeight = () => {
-    const { taskListContainer } = this.refs
-    const containerHeight = taskListContainer.clientHeight
+        if (!draggingResources.destination) {
+            return
+        } else if (reason === 'DROP') {
+            this.props.reorderTasks(
+                this.props.deleted,
+                this.props.deleted ? this.props.archivedTasks : this.props.tasks,
+                source,
+                destination,
+                draggableId,
+                this.props.projectId
+            )
+        }
+    }
 
-    this.setState(({open}) => ({
-      open: !open
-    }), () => {
-        taskListContainer.style.height = this.state.open ? 'auto' : 0;
-    })
-  }
+    sortTasksByOrder = (a, b) => a.order - b.order
 
-  render() {
-    const {
-      projectId,
-      deleted,
-      label,
-      tasks,
-      archivedTasks
-    } = this.props
-    const { open } = this.state
+    toggleHeight = () => {
+        const { taskListContainer } = this.refs
+        const containerHeight = taskListContainer.clientHeight
 
-    const taskList = deleted ? archivedTasks : tasks
+        this.setState(({ open }) => ({
+            open: !open
+        }), () => {
+            taskListContainer.style.height = this.state.open ? 'auto' : 0
+        })
+    }
 
-    return (
-      <div className="task-list-container">
-        <div className="toggle-height" onClick={this.toggleHeight}>
-          <div className="toggle-height-trigger">
-            <ChevronRight size={12} className={open ? 'open' : 'closed'} />
-          </div>
-          <div className="label">{label}</div>
-        </div>
+    render() {
+        const {
+            projectId,
+            deleted,
+            label,
+            tasks,
+            archivedTasks
+        } = this.props
+        const { open } = this.state
 
-        <div className={`task-list ${open ? 'open ' : 'closed'}`} ref="taskListContainer">
-          <DragDropContext onDragEnd={this.reOrderOnDragEnd}>
-            <Droppable droppableId="droppable-1">
-              {
-                (provided) => (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                  >
-                    {
-                      taskList !== undefined &&
-                      taskList
-                        .filter(task => deleted ? task.status === 'DONE' : task.status !== 'DONE')
-                        .filter(this.filterByQuery)
-                        .sort(this.sortTasksByOrder)
-                        .map((task, idx) => (
-                          <Draggable key={task._id} draggableId={task._id} index={idx} isDragDisabled={projectId === 'all'}>
-                            {(provided, { isDragging }) => (
-                              <Task
-                                isDragging={isDragging}
-                                task={task}
-                                provided={provided}
-                                archived={deleted}
-                              />
-                            )}
-                          </Draggable>
-                        ))
-                    }
-                    {provided.placeholder}
-                  </div>
-                )
-              }
-            </Droppable>
+        const taskList = deleted ? archivedTasks : tasks
 
-            {!deleted && <AddTask />}
-          </DragDropContext>
-        </div>
-      </div>
-    )
-  }
+        return (
+            <div className="task-list-container">
+                <div className="toggle-height" onClick={this.toggleHeight}>
+                    <div className="toggle-height-trigger">
+                        <ChevronRight size={12} className={open ? 'open' : 'closed'} />
+                    </div>
+                    <div className="label">{label}</div>
+                </div>
+
+                <div className={`task-list ${open ? 'open ' : 'closed'}`} ref="taskListContainer">
+                    <DragDropContext onDragEnd={this.reOrderOnDragEnd}>
+                        <Droppable droppableId="droppable-1">
+                            {
+                                (provided) => (
+                                    <div
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                    >
+                                        {
+                                            taskList !== undefined &&
+                                            taskList
+                                                .filter((task) => deleted ? task.status === 'DONE' : task.status !== 'DONE')
+                                                .filter(this.filterByQuery)
+                                                .sort(this.sortTasksByOrder)
+                                                .map((task, idx) => (
+                                                    <Draggable
+                                                        key={task._id}
+                                                        draggableId={task._id}
+                                                        index={idx}
+                                                        isDragDisabled={projectId === 'all'}>
+                                                        {
+                                                            (provided, { isDragging }) => (
+                                                                <Task
+                                                                    isDragging={isDragging}
+                                                                    task={task}
+                                                                    provided={provided}
+                                                                    archived={deleted}
+                                                                />
+                                                            )
+                                                        }
+                                                    </Draggable>
+                                                ))
+                                        }
+                                        {provided.placeholder}
+                                    </div>
+                                )
+                            }
+                        </Droppable>
+
+                        {!deleted && <AddTask />}
+                    </DragDropContext>
+                </div>
+            </div>
+        )
+    }
 }
 
 TaskList.defaultProps = {
-  tasks: [],
-  archivedTasks: [],
-  deleted: false
+    tasks: []
 }
 
 TaskList.propTypes = {
-  tasks: PropTypes.instanceOf(Array),
-  searchQuery: PropTypes.string,
-  filterStatus: PropTypes.string
+    tasks: PropTypes.instanceOf(Array),
+    searchQuery: PropTypes.string,
+    filterStatus: PropTypes.string
 }
 
 
 const mapStateToProps = ({ projectReducer }) => ({
-  tasks: projectReducer.tasks,
-  archivedTasks: projectReducer.archivedTasks,
-  projectId: projectReducer.activeProjectId
+    tasks: projectReducer.tasks,
+    archivedTasks: projectReducer.archivedTasks,
+    projectId: projectReducer.activeProjectId
 })
 
 export default connect(mapStateToProps, { reorderTasks })(TaskList)
